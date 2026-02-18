eafb5b804c591133a540cb97dc8bbd1da1835d18
buzz <hitmanbuzz@proton.me>
2026-02-19 00:42:43 +0530

    add lua lsp to the package script

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -26,6 +26,7 @@ official_packages=(
     "vscode-html-languageserver"
     "vscode-css-languageserver"
     "typescript-language-server"
+    "lua-language-server"
     "gopls"
     "clang"
     "nmap"
