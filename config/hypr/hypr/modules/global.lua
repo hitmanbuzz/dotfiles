608a453fbebcd6977f936446cfa7574f395c97f6
hitmanbuzz <hitmanbuzz@proton.me>
2026-05-12 08:11:08 +0530

    replace old hyprland config with the new lua config

---
+++ config/hypr/hypr/modules/global.lua
@@ -0,0 +1,7 @@
+-- Set global variable that is expose throughout hyprland config
+
+TERMINAL = "alacritty"
+BROWSER = "helium-browser"
+FILE_MANAGER = "thunar"
+LAUNCHER = "rofi -show drun"
+POWER_MENU = "wlogout --protocol layer-shell"
---
+++ config/hypr/hypr/modules/input.lua
@@ -0,0 +1,29 @@
+hl.config({
+    input = {
+        kb_layout  = "us",
+        kb_variant = "",
+        kb_model   = "",
+        kb_options = "",
+        kb_rules   = "",
+
+        follow_mouse = 1,
+
+        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+
+        touchpad = {
+            natural_scroll = false,
+        },
+    },
+})
+
+hl.gesture({
+    fingers = 3,
+    direction = "horizontal",
+    action = "workspace"
+})
+
+hl.device({
+    name        = "lenovo-lenovo-legion-m300-rgb-gaming-mouse",
+    sensitivity = -0.6,
+})
+
